4af772d5fa7124930b7325283c9c71e9d13b4375
Kleanta <kleasina98@gmail.com>
2025-05-19 22:07:38 +0100

    m

--- client.py
+++ client.py
@@ -1,61 +1,36 @@
 import requests
 import json
 
+
 class SpamDetectorClient:
+    def _init_(self, server_url="http://localhost:5000"):
+        self.server_url = server_url
 
-    def test_predict_spam():
-        url = "http://127.0.0.1:5000/model/email"
+    def info(self):
+        info_url = ''
+        api_url = f"{self.server_url}/{info_url}"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return {}
 
-        headers = {"Content-Type": "application/json; charset=utf-8"}
+    def detect_one(self, email_text: str):
+        detect_one_url = ''
+        api_url = f"{self.server_url}/{detect_one_url}"
+        payload = {}
+        return False
 
-        data = {
-            "email_text": "o dimi einai malakas",
-        }
-
-        response = requests.post(url, headers=headers, json=data)
-
-        print("Status Code", response.status_code)
-        print("JSON Response ", response.json())
-
-        assert response.status_code==200, "Status code not ) "
-        assert response.json()=={'class': 'spam'}
-
-    def test_predict_not_spam():
-        url = "http://127.0.0.1:5000/model/email"
-
-        headers = {"Content-Type": "application/json; charset=utf-8"}
-
-        data = {
-            "email_text": "jm URL justin mason writes except for NUMBER thing defanged mime messages that s a big problem but if you didn t just remove the headers and instead reverted back to the x spam prev versions it d more or less work btw fixed the downloads page check now un defangs mime it was screwing up some of my mass check results where sa markup was present yes if there ever was a warning about sa markup in mass check it never worked for me dan ",
-        }
-
-        response = requests.post(url, headers=headers, json=data)
-
-        print("Status Code", response.status_code)
-        print("JSON Response ", response.json())
-
-        assert response.status_code==200, "Status code not ) "
-        assert response.json()=={'class': 'not_spam'}
+    def detect_many(self, email_texts: list[str]):
+        detect_many_url = ''
+        api_url = f"{self.server_url}/{detect_many_url}"
+        payload = {}
+        return [False] * len(email_texts)
 
 
-    def test_predict_emails_not_spam():
-        url = "http://127.0.0.1:5000/model/emails"
-
-        headers = {"Content-Type": "application/json; charset=utf-8"}
-
-        data = {
-            "email_texts": ["jm URL justin mason writes except for NUMBER thing defanged mime messages that s a big problem but if you didn t just remove the headers and instead reverted back to the x spam prev versions it d more or less work btw fixed the downloads page check now un defangs mime it was screwing up some of my mass check results where sa markup was present yes if there ever was a warning about sa markup in mass check it never worked for me dan ","Adevrtise by this with discount"]
-        }
-
-        response = requests.post(url, headers=headers, json=data)
-
-        print("Status Code", response.status_code)
-        print("JSON Response ", response.json())
-
-        assert response.status_code==200, "Status code not ) "
-        assert response.json()=={'classes':["not_spam","not_spam"]}
-
-    if __name__=="__main__":
-        test_predict_emails_not_spam()
-        test_predict_spam()
-        test_predict_not_spam()
+if _name_ == '_main_':
+    client = SpamDetectorClient()
+    print(client.info())
+    print(client.detect_one("This is a test email."))
+    print(client.detect_many(["This is a test email.", "Another test email."]))
